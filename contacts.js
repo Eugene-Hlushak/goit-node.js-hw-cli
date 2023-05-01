789e381457927957b25c07315e5494d504214b76
Eugene Hlushak <hlushak.e@gmail.com>
2023-05-02 00:13:37 +0300

    add error handling

--- contacts.js
+++ contacts.js
@@ -14,36 +14,57 @@ const getAllContacts = async () => {
   return JSON.parse(buffer);
 };
 
-const writeFile = async (path, file) =>
+const writeFile = async (path, file) => {
   await fs.writeFile(path, JSON.stringify(file));
+  const newList = await getAllContacts();
+  console.table(newList);
+};
+
+const sorryLog = (contactId) =>
+  console.log(`Sorry, no contact found with this id '${contactId}'`);
 
 async function listContacts() {
-  const allContacts = await getAllContacts();
-  console.table(allContacts);
+  try {
+    const allContacts = await getAllContacts();
+    console.table(allContacts);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function getContactById(contactId) {
-  const allContacts = await getAllContacts();
-  const getContact = allContacts.find((contact) => contact.id === contactId);
-  console.table(getContact);
+  try {
+    const allContacts = await getAllContacts();
+    const getContact = allContacts.find((contact) => contact.id === contactId);
+    if (!getContact) return sorryLog(contactId);
+    console.table(getContact);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const allContacts = await getAllContacts();
-  const newContactsList = allContacts.filter(
-    (contact) => contact.id !== contactId
-  );
-  await writeFile(contactsPath, newContactsList);
-  const newList = await getAllContacts();
-  console.table(newList);
+  try {
+    const allContacts = await getAllContacts();
+    const deleted = allContacts.find((contact) => contact.id === contactId);
+    if (!deleted) return sorryLog(contactId);
+    const newContactsList = allContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await writeFile(contactsPath, newContactsList);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function addContact(name, email, phone) {
-  const allContacts = await getAllContacts();
-  const newContactsList = [...allContacts, { name, email, phone }];
-  await writeFile(contactsPath, newContactsList);
-  const newList = await getAllContacts();
-  console.table(newList);
+  try {
+    const allContacts = await getAllContacts();
+    const newContactsList = [...allContacts, { name, email, phone }];
+    await writeFile(contactsPath, newContactsList);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports = {
